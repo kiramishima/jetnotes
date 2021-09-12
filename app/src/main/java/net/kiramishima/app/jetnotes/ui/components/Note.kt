0b92f07d7b8c1ae34f8ebc7cf89a38ed7ee65228
Paul <kiramishima@outlook.com>
2021-09-12 14:08:50 -0500

    Formato a Item Nota

--- app/src/main/java/net/kiramishima/app/jetnotes/ui/components/Note.kt
+++ app/src/main/java/net/kiramishima/app/jetnotes/ui/components/Note.kt
@@ -2,28 +2,44 @@ package net.kiramishima.app.jetnotes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.kiramishima.app.jetnotes.theme.rwGreen
 
 @Composable
-@Preview
 fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+    ) {
+        NoteColor(
+            color = rwGreen,
+            size = 40.dp,
+            padding = 4.dp,
+            border = 1.dp
         )
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Title", maxLines = 1)
             Text(text = "Content", maxLines = 1)
         }
-        Checkbox(checked = false, onCheckedChange = {}, modifier = Modifier.padding(start = 8.dp))
+        Checkbox(
+            checked = false,
+            onCheckedChange = {},
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
 
